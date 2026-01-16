3e66b323d6c37c8003d27043e50955f1230cf8af
katarina-pen <katarinapenava0711@gmail.com>
2026-01-16 16:46:12 +0100

    v.3

--- handout/1 - example_requests/lib/request.rb
+++ handout/1 - example_requests/lib/request.rb
@@ -2,30 +2,17 @@
 class Request
 
   def initialize (request_string)
-    @daString = request_string.split("\r\n")
+    line, *lines = request_string.split(/\r?\n/)
+    method, resource, version = line.split
+    p method
+    p resource
+    p lines
 
-  end
   
-  def method
-    @method = daString[0]
-  end
-
-  def resource
-    @resource =daString[1]
-  end
-
-  def version
-    @version = daString[2]
-  end
-
-  def headers
-    @headers = daString[3]
-  end
-
-  def params
-    @params = daString[4]
   end
 
   
 
 end
+
+Request.new(File.read("get-examples.request.txt"))
