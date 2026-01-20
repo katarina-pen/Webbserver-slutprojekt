bb5c48d7cea70b86f822ddbc333eab107e7850c6
katarina-pen <katarinapenava0711@gmail.com>
2026-01-20 15:11:10 +0100

    v. 4

--- handout/1 - example_requests/lib/request.rb
+++ handout/1 - example_requests/lib/request.rb
@@ -2,11 +2,33 @@
 class Request
 
   def initialize (request_string)
+
     line, *lines = request_string.split(/\r?\n/)
-    method, resource, version = line.split
-    p method
-    p resource
+   
+    # p line.split
+    # @method, @resource, @version = line.split
+    # p @method
+    # p @resource
+    # p @version
+    
     p lines
+    
+    headers = {
+    }
+
+    key, value = lines[0].split(": ")
+    headers[key] = value
+    
+    p headers
+
+    i = 0
+    p i
+    while i > lines.length
+      
+      puts lines[i]
+      i = i + 1
+    end
+    
 
   
   end
