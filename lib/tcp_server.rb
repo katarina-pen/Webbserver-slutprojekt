7eb88bdf3ccf68b185d516e5b08177272ed77394
katarina-pen <katarinapenava0711@gmail.com>
2026-02-20 16:48:18 +0100

    v.8

--- lib/tcp_server.rb
+++ lib/tcp_server.rb
@@ -30,19 +30,40 @@ class HTTPServer
 
 
       # kolla om det finns någon sån fil öht
-      if File.exist?("./lib/#{request.resource}")
-      #if request.resource == "/hello.html"
-        # html = "<h1>Hello, World!</h1>
-        #         <h4> hi </h4>"
+      maybe_file = "./lib/#{request.resource}"
+      if File.exist?(maybe_file)
+        p "-------+++++++++----------------------------"
+        p "maybe_file split etc lagra värdet"
+        p "Filen:#{maybe_file}"
+        #ganska scuffed solution, ÄNDRA sen!!!!!!!
+        filtyp= maybe_file.split(".")
+        p "Filtyp, typ:#{filtyp[2]}"
+        #Checkar content type
+        #images
+        if filtyp[2] == "png"
+          content_type ="image/png"
+        elsif filtyp[2] == "jpg"
+          content_type = "text/jpeg"
+        #text
+        elsif filtyp[2] == "css"
+          content_type = "text/css"
+        elsif filtyp[2] == "html"
+          content_type = "text/html"
+        elsif filtyp[2] == "js"
+          content_type = "text/javascript"
+        end
+
+        
         @resource = request.resource
-        html = File.read("./lib/#{@resource}")
+        body = File.read("./lib#{@resource}")
         
         #Vad var det för typ fil?
         
+
         content_type = "text/html"
         status = "200 OK"
       else
-        html = "<h1>WAT</h1>
+        body = "<h1>WAT</h1>
                 <h2>Could not find page<h2>"
         status = "404 NOT FOUND"
       end
@@ -51,7 +72,7 @@ class HTTPServer
       session.print "HTTP/1.1 #{status}\r\n"
       session.print "Content-Type: #{content_type}\r\n"
       session.print "\r\n"
-      session.print html
+      session.print body
       session.close
     end
   end
